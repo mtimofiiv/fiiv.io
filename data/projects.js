b6ab0643cff0cfc11f917408fc3d46d2a4a15fe2
fiiv <git@fiiv.io>
2022-12-22 06:43:24 +0000

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -7,24 +7,10 @@ export default [
     description: 'An easy-to-use web service to reliably serve high quality logos of any website, using favicons under the hood.'
   },
   {
-    title: '30 Wears',
-    subtitle: 'iOS/Android app',
-    uri: 'https://30wears.app',
-    icon: 'https://icon.horse/icon/30wears.app',
-    description: 'An app to help you track your wardrobe, and get as many wears out of each garment as possible!'
-  },
-  {
-    title: 'Meeting Canary',
-    subtitle: 'Web app',
-    uri: 'https://meetingcanary.app',
-    icon: 'https://icon.horse/icon/meetingcanary.app',
-    description: 'Invite Meeting Canary to your next meeting, and get the best meeting note-taking and task manager there is.'
-  },
-  {
-    title: 'State of the Globe',
-    subtitle: 'Web app',
-    uri: 'https://stateoftheglobe.com',
+    title: 'st8oftheglobe',
+    subtitle: 'Youtube Channel',
+    uri: 'https://youtube.com/@st8oftheglobe',
     icon: 'https://icon.horse/icon/stateoftheglobe.com',
-    description: 'I\'m working on a project to illustrate the current status quo of our world, through data, visualisation and storytelling.'
+    description: 'I\'m making documentaries out of weird stories.'
   }
 ]
