235479f2715b129ce412298e793cea4360f991b9
fiiv <git@fiiv.io>
2025-02-20 14:11:40 +0700

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,14 @@
 export default [
   {
+    title: 'Bangkok Rent Map',
+    subtitle: 'Web Application',
+    uri: 'https://bkkrentmap.com',
+    icon: 'https://icon.horse/icon/bkkrentmap.com',
+    description: 'A breakdown of Bangkok rental prices based on various datasets.'
+  },
+  {
     title: 'Icon Horse',
-    subtitle: 'Web service',
+    subtitle: 'Web Service',
     uri: 'https://icon.horse',
     icon: 'https://icon.horse/icon/icon.horse',
     description: 'An easy-to-use web service to reliably serve high quality logos of any website, using favicons under the hood.'
